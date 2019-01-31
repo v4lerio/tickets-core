2892e785765badd2195d0f5c1f88e7eabaaf7601
Tom Harper <tom@sticktech.co.uk>
2019-01-31 20:07:56 +0000

    scaffold for spa

---
+++ resources/js/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Hello from './components/Hello'
+import Home from './components/Home'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+        },
+    ],
+});
