1428d57c3cd693329457f0f7f976067c9ec07ff7
Tom Harper <tom@sticktech.co.uk>
2019-02-09 19:35:41 +0000

    Tidy customer edit component

--- resources/js/components/customers/edit.vue
+++ resources/js/components/customers/edit.vue
@@ -8,19 +8,8 @@
 <script>
     import customer_form from './partials/customer_form'
     export default {
-        data() {
-            return {
-                customer: {}
-            }
-        },
         components: {
             customer_form
-        },
-        created() {
-            this.axios.get('/api/customers/' + this.$route.params.id)
-            .then(response => {
-                this.customer = response.data.data;
-            })
         }
     }
 </script>
