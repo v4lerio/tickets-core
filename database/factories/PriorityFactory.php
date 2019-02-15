d0cd430f0a1e2d1e013fa7d11c29f8b89d7ce407
Tom Harper <tom@sticktech.co.uk>
2019-02-15 19:34:38 +0000

    Update Priority Factory

--- database/factories/PriorityFactory.php
+++ database/factories/PriorityFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Priority::class, function (Faker $faker) {
     return [
-        'name' => $this->faker->name,
+        'name' => $this->faker->word,
         'colour' => $this->faker->hexcolor,
         'urgency' => $this->faker->randomDigit,
     ];
